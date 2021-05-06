95fcbf7c502234f20f0762768468b95ccfc1b792
AnaGutson <83771013+AnaGutson@users.noreply.github.com>
2021-05-06 11:12:24 -0300

    Add files via upload

---
+++ constantes.h
@@ -0,0 +1,54 @@
+#define FIN  			"\033[0m"
+#define BLANCO 			"\033[1m\033[37m"
+#define AZUL 			"\033[1m\033[34m"
+#define ROJO 			"\033[1m\033[31m"
+#define MAGENTA 		"\033[1m\033[35m"
+#define AMARILLO 		"\033[1m\033[33m"
+#define PUNTO_NEGRO 	"\254"
+#define CUADRADO_BLANCO "\250"
+#define SANGRIA 		"         "
+#define LINEA 			"\n-------------------------------------------------\n"
+
+#define ENSALADA 	'E'
+#define HAMBURGUESA 'H'
+#define PIZZA 		'P'
+#define GUISO 		'G'
+
+#define PRIMER_PERSONAJE 	"Legolas"
+#define PRIMER_EQUIPO 		"los elfos"
+#define SEGUNDO_PERSONAJE 	"Gimli"
+#define SEGUNDO_EQUIPO 		"los enanos"
+
+const int DIA_MES_MIN = 1;
+const int DIA_MES_MAX = 30;
+const int HORAS_SUENO_MIN = 0;
+const int HORAS_SUENO_MAX = 12;
+const int MUCHO_VIENTO = 75;
+const int VIENTO_REGULAR = 50;
+const int POCO_VIENTO = 25;
+const int MUCHA_HUMEDAD = 75;
+const int HUMEDAD_REGULAR = 50;
+const int POCA_HUMEDAD = 25;
+const int PUNTOS_PIE_DERECHO = 10;
+const int PUNTOS_ENSALADA = 20;
+const int PUNTOS_HAMBURGUESA = 15;
+const int PUNTOS_PIZZA = 10;
+const int PUNTOS_GUISO = 5;
+const int PUNTOS_DORMIR_NORMAL = 10;
+const int PUNTOS_DORMIR_MUCHO = 20;
+const int MAX_PUNTOS_ANIMO_MALO = 20;
+const int MAX_PUNTOS_ANIMO_REGULAR = 35;
+const int MIN_DORMIR_NORMAL = 5;
+const int MAX_DORMIR_NORMAL = 8;
+const int MAX_DIA_MUCHO_VIENTO = 10;
+const int MAX_DIA_VIENTO_REGULAR = 20;
+const int CANT_OPC_PARENTESIS_MENSAJE = 5;
+const int MAX_RANDOM = 12;
+const char MANANA = 'M';
+const char TARDE = 'T';
+const char NOCHE = 'N';
+const char PIE_DERECHO = 'D';
+const char PIE_IZQUIERDO = 'I';
+const char MALO = 'M';
+const char REGULAR = 'R';
+const char BUENO = 'B';
